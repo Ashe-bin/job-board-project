598bbcf30d48e2730cd5d431d13f9f4f3c67c8c9
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2025-02-01 17:12:49 +0300

    feat: integrate Arcjet for security enhancements

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -5,9 +5,22 @@ import { checkSessionExits } from "../utils/checkSessionExits";
 import { companySchema, jobSeekerSchema } from "../utils/zodSchema";
 import { prisma } from "../utils/db";
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "../utils/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(shield({ mode: "LIVE" }))
+  .withRule(detectBot({ mode: "LIVE", allow: [] }));
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const session = await checkSessionExits();
+
+  const req = await request();
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
   const validateData = companySchema.parse(data);
   await prisma.user.update({
     where: { id: session.id },
@@ -21,6 +34,13 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
 }
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const session = await checkSessionExits();
+
+  const req = await request();
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
   const validateData = jobSeekerSchema.parse(data);
   await prisma.user.update({
     where: { id: session.id },
